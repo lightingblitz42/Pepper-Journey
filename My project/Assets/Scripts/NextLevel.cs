2fe0a25e70845e3dc6bc4748d86e34645cc5c63a
Andrew <Andrew.malaska@gmail.com>
2025-07-19 18:28:51 -0700

    6

---
+++ My project/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    public bool teleported = false;
+    public Transform goTo;
+    public Transform CamGoTo;
+    public float blackScreen = 0;
+    public float ne = 1; 
+    public SpriteRenderer blackSc;
+    public GameObject g;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blackSc.color = new Color(0, 0, 0, blackScreen);
+        if(blackScreen > 0 || ne == -1)
+        {
+            blackScreen -= Time.deltaTime * ne * 2;
+            if(blackScreen > 1 && teleported == false)
+            {
+                g.transform.position = goTo.position;
+                Camera.main.transform.position = new Vector3(CamGoTo.position.x, CamGoTo.position.y, -10);
+                teleported = true;
+            }
+            if(blackScreen > 1.3f)
+            {
+                ne = 1;
+                teleported = false;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            ne = -1;
+            g = collision.gameObject;
+        }
+    }
+}
