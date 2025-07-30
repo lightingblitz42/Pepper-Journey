e5f5674de5cb6e2946f8816c391543a8570343dc
Andrew <Andrew.malaska@gmail.com>
2025-07-30 04:11:18 -0700

    d

---
+++ My project/Assets/Scripts/Activate.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate : MonoBehaviour
+{
+    public List<GameObject> g = new List<GameObject>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            for(int i = 0; i < g.Count; i++)
+            {
+                g[i].SetActive(true);
+            }
+        }
+    }
+}
